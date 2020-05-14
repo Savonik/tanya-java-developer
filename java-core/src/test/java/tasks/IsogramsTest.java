d8ca6f2db6f782bea6d6ba9401ed225dc1f2ec68
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-05-15 00:12:34 +0300

    Created tests fot isogram and migrated project to maven

--- java-core/src/test/java/tasks/IsogramsTest.java
+++ java-core/src/test/java/tasks/IsogramsTest.java
@@ -7,9 +7,15 @@ public class IsogramsTest {
     @Test
     public void testNotIsogram(){
         Assert.assertFalse(Isograms.isIsogram("ooo"));
+        Assert.assertFalse("Eugene is not isogram", Isograms.isIsogram("Eugene"));
     }
     @Test
     public void testIsEmptyIsogram(){
         Assert.assertTrue(Isograms.isIsogram(""));
+    }
+    @Test
+    public void testIsogram(){
+        Assert.assertTrue(Isograms.isIsogram("Tany"));
+        Assert.assertTrue(Isograms.isIsogram("Lida"));
     }
 }
